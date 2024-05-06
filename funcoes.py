bf521c9a2de9c8bac1101fe9f373fb7dc436d808
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-05-06 16:25:51 -0300

    a

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+
+def validador(cpf):
+    cpf_digitado = int(input('CPF: '))
+    nove_digitos = cpf_digitado[:9]
+    contador_regressivo = 10
+    resultado = 0
+    for digito in cpf_digitado:
+        resultado += (digito * contador_regressivo)
+        contador_regressivo -= 1
+    digito = digito if digito >= 9 else 0
+        
---
+++ menu.py
@@ -0,0 +1,16 @@
+import funcoes as validador
+
+print('Bem vindo ao gerador e validador de CPF')
+
+print('_________________________________________________')
+
+print('Escolha uma das opções [1]validar cpf, [2]gerar cpf [3]sair.')
+
+
+opcao = int(input(': '))
+
+if opcao == 1:
+   
+
+
+    
