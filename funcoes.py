15984b88eb87bcfe483f84b2a648b1b57ce74ac3
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-05-21 18:15:04 -0300

    class/ funcoes
    criando as classses e funcoes do projeto :)

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,40 @@
 
-def validador(cpf):
-    cpf_digitado = int(input('CPF: '))
-    nove_digitos = cpf_digitado[:9]
-    contador_regressivo = 10
-    resultado = 0
-    for digito in cpf_digitado:
-        resultado += int(digito) * contador_regressivo
-        contador_regressivo -= 1
-    digito = digito if digito <= 9 else 0
+
+
+class Pessoa:
+
+    def __init__(self,nome,sobrenome,idade,entrada= True) -> None:
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+        self.entrada = entrada
+
+    def verificacao(self, entrada ):
+        if self.idade < 18:
+            print(f'Senhor {self.nome}, infelizmente somente maiores de idade pode usar o programa')
+            self.entrada = False
+            return self.entrada
+        
+    def entrada_sistema(self):
+        if self.verificacao():
+            print(f'{self.nome} {self.sobrenome} entrou no sistema..')
+        else:
+            print(f'{self.nome} {self.sobrenome} não pode entrar no sistema..')
+    
+
+class Validador_cpf:
+    def __init__(self, cpf):
+        self.cpf = cpf
+    
+
+    def validador(self):
+        qtd_digitos_permitido = 11
+        if self.cpf > qtd_digitos_permitido:
+            print('Infelizmente o cpf foi digitado incorreto, reinicie o programa...')
+        else:
+            while True:
+                self.cpf[:9]
+                for digito in self.cpf:
+                    print(digito)
+
+
