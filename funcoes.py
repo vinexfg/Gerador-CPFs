be9284d5653da4d390a4b6b8b3e32df0eaf80eba
vinexfg <136178252+vinexfg@users.noreply.github.com>
2024-05-29 11:00:11 -0300

    funcoes
    finalizado

--- funcoes.py
+++ funcoes.py
@@ -1,44 +1,61 @@
-
-
+import random
 
 class Pessoa:
-
-    def __init__(self,nome,sobrenome,idade):
+    def __init__(self, nome, sobrenome, idade):
         self.nome = nome
         self.sobrenome = sobrenome
         self.idade = idade
-        self.entrada = None
-        self.verificacao
-        
+
+    @property
     def verificacao(self):
         if self.idade < 18:
-            print(f'Senhor {self.nome}, infelizmente somente maiores de idade pode usar o programa')
-            self.entrada = False
-            return self.entrada
-            
-        
-    def entrada_sistema(self):
-        if self.verificacao():
-            print(f'{self.nome} {self.sobrenome} entrou no sistema..')
-        else:
-            print(f'{self.nome} {self.sobrenome} não pode entrar no sistema..')
-    
+            print(f'Senhor(a) {self.nome}, infelizmente somente maiores de idade podem usar o programa')
+            return False
+        return True
 
-class Validador_cpf:
+    def entrada_sistema(self):
+        if self.verificacao:
+            print(f'{self.nome} {self.sobrenome} entrou no sistema.')
+        else:
+            print(f'{self.nome} {self.sobrenome} não pode entrar no sistema.')
+
+class ValidadorCPF:
     def __init__(self, cpf):
         self.cpf = cpf
-    
 
     def primeiro_digito(self):
-        self.cpf
-        for digito in range(self.cpf):
-            print(digito)
+        soma = sum(int(digito) * peso for digito, peso in zip(self.cpf[:9], range(10, 1, -1)))
+        resto = soma % 11
+        return 0 if resto < 2 else 11 - resto
 
-    def validador(self):
-        qtd_digitos_permitido = 11
-        if self.cpf > qtd_digitos_permitido:
-            print('...')
-        else:
-            pass
-            
+    def segundo_digito(self):
+        soma = sum(int(digito) * peso for digito, peso in zip(self.cpf[:10], range(11, 1, -1)))
+        resto = soma % 11
+        return 0 if resto < 2 else 11 - resto
+
+    def validar(self):
+        if len(self.cpf) != 11 or not self.cpf.isdigit():
+            print('CPF inválido. Deve conter 11 dígitos numéricos.')
+            return False
+        if int(self.cpf[-2]) == self.primeiro_digito() and int(self.cpf[-1]) == self.segundo_digito():
+            print('CPF válido.')
+            return True
+        print('CPF inválido.')
+        return False
+
+class GeradorCPF:
+    @staticmethod
+    def gerar():
+        nove_digitos = [random.randint(0, 9) for _ in range(9)]
+        cpf_parcial = ''.join(map(str, nove_digitos))
+
+        validador = ValidadorCPF(cpf_parcial + '00')
+        digito1 = validador.primeiro_digito()
+        cpf_com_digito1 = cpf_parcial + str(digito1)
+        
+        validador = ValidadorCPF(cpf_com_digito1 + '0')
+        digito2 = validador.segundo_digito()
+
+        cpf_final = cpf_com_digito1 + str(digito2)
+        return cpf_final
 
--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from funcoes import Pessoa, Validador_cpf
+from funcoes import Pessoa, Validador_cpf, GeradorCPF
 
 
 print('Bem vindo ao gerador e validador de CPF ')
@@ -8,4 +8,21 @@ while True:
     try:
         opcao = int(input('[1]validar [2]gerar [3]sair '))
         if opcao == 1:
+            cpf_input = input('Digite o CPF (apenas numeros): ')
+            validador = Validador_cpf(cpf_input)
+            validador.validar()
+        elif opcao == 2:
+            cpf_gerado = GeradorCPF.gerar()
+            print(f'CPF gerado: {cpf_gerado}')
+
+        elif opcao == 3:
+            print('saindo...')
+            break
+        else:
+            print('Opção invalida, tente novamente.')
+    except ValueError:
+        print('Entrada  invalida, por favor insira um numero')
+
+    
+
             
